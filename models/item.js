9b0819628ff452b63fedd76004de5ba928ab4517
mayra-rivas <mayrarivas1@hotmail.com>
2016-11-04 13:44:22 +0100

    damagejs

--- models/item.js
+++
@@ -1,7 +0,0 @@
-// Create a new mongoose schema
-var Schema = m.mongoose.Schema({
-  title: {type: String, required: true},
-  done: {type: Boolean, required: false}
-});
-
-module.exports = m.mongoose.model("Item", Schema);
