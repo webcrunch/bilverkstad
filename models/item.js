d55689fe5fdbb01c4e6cce88e8aaab1d34c53c93
Patricio Vergara Paredes <pato.vergara.p@gmail.com>
2016-11-14 14:32:25 +0100

    Removing item model bacause we dont need it

--- models/item.js
+++
@@ -1,9 +0,0 @@
-// Create a new mongoose schema
-var Schema = m.mongoose.Schema({
-  title: {type: String, required: true},
-  done: {type: Boolean, required: false}
-  // a relation
-  // doneBy: [{ type: m.mongoose.Schema.Types.ObjectId, ref: 'User' }]
-});
-
-module.exports = m.mongoose.model("Item", Schema);
