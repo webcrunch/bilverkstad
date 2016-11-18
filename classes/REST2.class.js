58527d88c2c58dd9f537c794e32e081660f9b060
mayra-rivas <mayrarivas1@hotmail.com>
2016-11-18 10:12:37 +0100

    UPDATE I MYSQL FUNKAR

--- classes/REST2.class.js
+++ classes/REST2.class.js
@@ -3,101 +3,103 @@ var s = g.settings;
 module.exports = class REST2 {
   constructor(express) {
     this.settings = s.REST2;
-     this.SQL = new g.classes.SQL(); // DB connection & models
-     this.app = express;
+    this.SQL = new g.classes.SQL(); // DB connection & models
+    this.app = express;
     this.router();
-    
+
   }
 
   // setup standard CRUD for route
   router() {
     var me = this;
     this.app.all(this.settings.route, function(req, res) {
-     
+
 
       // var table = me.SQL.getTable(req.params.table); // dont need to but we could check if there is an table in SQL with that name 
-      me.SQL.getTable(req.params.table, (response, error)=>{
+      me.SQL.getTable(req.params.table, (response, error) => {
         // do we have a 404?
         var resis = response;
-      
-      
-      if (!me[req.method] || !resis) {
-       res.sendStatus(404);
-        res.end();
-        return;
-      }
 
-      // // how to check if not logged in
-      // if (!req.session.loggedIn) { /*...*/ }
 
-      
-      
-    
-      me[req.method](req.params.table,req.params.tableID, req.body, req, res);
+        if (!me[req.method] || !resis) {
+          res.status(404);
+          res.end();
+          return;
+        }
+
+        
+        me[req.method](req.params.table, req.params.tableID, req.body, req, res);
+      });
     });
-     });
-      
-      
-      
+
   }
 
   // CREATE
-  POST(table,id, data, req, res) {
-  
-    var me  = this;
+  POST(table, id, data, req, res) {
+
+    var me = this;
+    console.log(table);
+    console.log(id);
+
     // if (!req.session.loggedIn){
     //   this.error({error: 'Login needed!'}, res); return;
     // }
     // else
-      me.SQL.INSERT(table,data,(response, error)=>{
+    me.SQL.INSERT(table, data, (response, error) => {
       // if(error){res.Status(404);res.json("fel");}
       // res.json(response);
       console.log(error);
       console.log(response);
       res.end();
-    });  
+    });
     // console.log(table, "table22", params, "params");
     // res.end();
   }
 
   // READ
-  GET(table,id, data, req, res) {
-      var me  = this;
-     
-    
-        me.SQL.GET(table,id, (response, error)=>{
-        if(error){res.sendStatus(400);return;}
-          res.json(response);
+  GET(table, id, data, req, res) {
+    var me = this;
 
-        });
-      
-      
-    }
 
-  
+    me.SQL.GET(table, id, (response, error) => {
+      if (error) {
+        res.status(400);
+        res.json(error);
+        return;
+      }
+      res.json(response);
+
+    });
+
+  }
+
 
   // UPDATE
-  PUT(table,id, data, req, res) {
-    // if (!req.session.loggedIn){
-    //   this.error({error: 'Login needed!'}, res); return;
-    // }
-    // else if (!params.modelID) { this.error({error: 'Missing ID!'}, res); return; }
-
-    // else
+  PUT(table, condition, data, req, res) {
     var me = this;
+    //console.log('hello');
+    console.log(condition);
+    console.log(data);
+
+    me.SQL.UPDATE(table, data, condition, (response, error) => {
+      if (error) {
+        res.status(404);
+        res.json(error);
+        return;
+      }
+      res.json(response);
+    });
+
   }
 
   // DELETE
-  DELETE(table,id, data, req, res) {
+  DELETE(table, id, data, req, res) {
     // if (!req.session.loggedIn){
     //   this.error({error: 'Login needed!'}, res); return;
     // }
     // else if (!params.modelID) { this.error({error: 'Missing ID!'}, res); return; }
-
     // else
-      var me = this;
-
-
+    var me = this;
   }
 
   error(err, res) {
--- classes/SQL.Class.js
+++ classes/SQL.Class.js
@@ -1,80 +1,114 @@
+// Code goes here
 'use strict';
 
 var connection;
 
 module.exports = class SQL {
-	constructor(){
-		this.settings = g.settings.SQL;
-		this.connection = m.mysql.createConnection({
-			host     : this.settings.host,
-			user     : this.settings.user,
-			password : this.settings.password,
-			database: this.settings.database
-			// port     : "3306"
-		}); 
-		this.connect();
+  constructor() {
+    this.settings = g.settings.SQL;
+    this.connection = m.mysql.createConnection({
+      host: this.settings.host,
+      user: this.settings.user,
+      password: this.settings.password,
+      database: this.settings.database
+        // port     : "3306"
+    });
+    this.connect();
 
 
-	}
+  }
 
-	connect() {
-	
-		this.connection.connect(function(err){
-			if(err){
-				console.log("Error connection to db- function");
-				console.log(err);
-			}
-			console.log("Mysql connected to bilverkstadDB");
-		});
+  connect() {
 
-	}
+    this.connection.connect(function(err) {
+      if (err) {
+        console.log("Error connection to db- function");
+        console.log(err);
+      }
+      console.log("Mysql connected to bilverkstadDB");
+    });
 
-	getTable(table,cb){
-		this.connection.query('SHOW TABLES LIKE "' + table + '"'  ,(err, data) =>{
-			if(err) {cb(false);}
-				cb(true);
-		});
+  }
 
-	}
+  getTable(table, cb) {
+    this.connection.query('SHOW TABLES LIKE "' + table + '"', (err, data) => {
+      if (err) {
+        cb(false);
+      }
+      cb(true);
+    });
 
-	GET(table,id, cb){
-		console.log("table", table, "id", id);
-		if(id){
+  }
 
-			this.connection.query("SELECT * FROM ?? WHERE SSN =?" , [table,id]  ,(err, data) =>{
-				if(err){cb(err);return;}
-				cb(data);
-			});
-		}
-		else{
+  GET(table, id, cb) {
+    console.log("table", table, "id", id);
+    if (id) {
 
-			this.connection.query('SELECT * FROM ??', table  ,(err, data) =>{
-				if(err){cb(err);return;}
-				cb(data);
-			});
-		}
-	}
+      this.connection.query("SELECT * FROM ?? WHERE SSN =?", [table, id], (err, data) => {
+        if (err) {
+          cb(err);
+          return;
+        }
+        cb(data);
+      });
+    } else {
+
+      this.connection.query('SELECT * FROM ??', table, (err, data) => {
+        if (err) {
+          cb(err);
+          return;
+        }
+        cb(data);
+      });
+    }
+  }
 
 
 
-	INSERT(tableName,data,cb){
-		console.log(tableName);
-		console.log(data);//' + tableName + '
-		this.connection.query('INSERT INTO customer  SET  ?' , data ,(err, data) =>{
-			if(err){cb(err);}
-			cb(data);
-		});
-		
-	}
+  INSERT(tableName, data, cb) {
+    console.log(tableName);
+    console.log(data); //' + tableName + '
+    this.connection.query('INSERT INTO customer  SET  ?', data, (err, data) => {
+      if (err) {
+        cb(err);
+      }
+      cb(data);
+    });
 
-	DELETE(query,cb){
-		this.connection.query('SHOW TABLES LIKE "' + table + '"'  ,(err, data) =>{});
-	}
+  }
 
-	UPDATE(query,cb){
-		this.connection.query('SHOW TABLES LIKE "' + table + '"'  ,(err, data) =>{});
-	}
+// Fick detta att fungera med hjälp av Thomas
 
-	
-	
+  UPDATE(table, data, condition, cb) {
+    var sql1 = 'UPDATE `' + table + '` SET ';
+    var paramVals = [];
+
+    var firstParam = true;
+    for(var i in data){
+      if(!firstParam){ sql1 += ', '; }
+      if(firstParam){ firstParam = false; }
+      sql1 += '`' + i + '`' + ' = ? ';
+      paramVals.push(data[i]);
+    }
+
+    sql1 += ' WHERE SSN = ?';
+
+    paramVals.push(condition);
+
+    console.log("SQL",sql1,"paramVals",paramVals);
+    this.connection.query(sql1, paramVals, (err, data) => {
+      console.log("HERE RESULT",err,data);
+      cb(data,err);
+    });
+    //this.connection.query('UPDATE `' + table + '` SET ? =  WHERE SSN = ' + condition + '', data ,(err, data) =>{
+    //	if(err){cb(err);}
+    //	cb(data);
+
+  }
+
+DELETE(query, cb) {
+    this.connection.query('SHOW TABLES LIKE  "' + table + '"', (err, data) => {});
+  }
+
+
 };
